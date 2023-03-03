758dc060c246d2bfdd92f562d76d886bcfa7cbfb
Adriel Minyety Gonzalez <44579415+AdrielMinyety@users.noreply.github.com>
2023-03-03 12:12:24 -0800

    new browserPolicies video/music autoplay-updated

--- audio.js
+++ audio.js
@@ -1,4 +1,5 @@
 var reproductor = document.getElementById("music");
+var playButton = document.getElementById("iniciar");
 
 var playList = {
     music1:"audio/DarkRain-IMOJIC.mp3",
@@ -9,8 +10,11 @@ reproductor.audioTracks = playList;
 
 reproductor.src = playList.music1;
 reproductor.volume = 0.6;
-reproductor.play();
-
+// detect when user start
+playButton.addEventListener("click", function() {
+    reproductor.play();
+});
+// after song end, play next one
 reproductor.onended = function () {   
     reproductor.src = playList.music2;
     reproductor.volume = 0.5;
